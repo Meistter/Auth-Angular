8c075e31953246c8d54a1d342e7646e0008d72a9
Meistter <josueperez723@gmail.com>
2023-05-08 14:36:21 -0400

    registro y logueo junto

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
+import { switchMap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +16,12 @@ export class AuthService {
 
   register(name:string, email: string, password: string){
     return this.http.post(`${this.API}/api/v1/auth/register`,{name,email,password})
+  }
+  registerAndLogin(name:string, email: string, password: string){
+    return this.register(name, email, password)
+    .pipe(
+      switchMap(() => this.login(email, password))
+    )
   }
   isAvailable(email: string){
     return this.http.post<{isAvailable : boolean}>(`${this.API}/api/v1/auth/is-available`,{email})
