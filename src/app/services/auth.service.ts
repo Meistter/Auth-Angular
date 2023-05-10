6a755649e033960453feaf53245932ebe6173c83
Josue Perez <josueperez723@gmail.com>
2023-05-09 20:42:37 -0400

    guardado de token en LS

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
+import { TokenService } from './token.service';
+import { ResponseLogin } from '@models/auth.model';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
   API = environment.API_URL
 
   login(email: string, password: string){
-    return this.http.post(`${this.API}/api/v1/auth/login`,{email,password}) 
+    return this.http.post<ResponseLogin>(`${this.API}/api/v1/auth/login`,{email,password}) 
+    .pipe(
+      tap(response=>{
+        this.tokenService.saveToken(response.access_token)
+      })
+    )
   }
 
   register(name:string, email: string, password: string){
